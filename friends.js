baf3e745d904d464be3f1e6e2c221682275f8b20
Alexis Jones <shyniece21@yahoo.com>
2022-12-11 13:40:11 -0700

    Add answers

--- friends.js
+++ friends.js
@@ -1,6 +1,7 @@
 var numbers = [30, 10, 40, 5, 17, 22, 19, 45]
 for (var i = 0; i < numbers.length; i++) {
-    if (numbers[i] > 30){ 
+    if (numbers[i] > 30 || numbers[i]< 60){ 
         console.log(numbers[i]);
     }
 }
+
